f81f819dc645b4c220e8816e4eff2710a7963b4d
CodeForFame <codeforfame@gmail.com>
2012-03-17 21:16:27 -0500

    Fixing error from last commit

--- LoginServer/src/org/moparscape/msc/ls/persistence/impl/SerializedStorageMedium.java
+++ LoginServer/src/org/moparscape/msc/ls/persistence/impl/SerializedStorageMedium.java
@@ -242,7 +242,7 @@ public class SerializedStorageMedium implements StorageMedium {
 		}
 
 		ps.setOwner((int) ownerId, 1, 0L);
-		ps.setLogin(ps.getLastLogin(), "127.0.0.1");
+		ps.setLogin(ps.getLastLogin(), 0L;
 
 		return ps;
 	}
