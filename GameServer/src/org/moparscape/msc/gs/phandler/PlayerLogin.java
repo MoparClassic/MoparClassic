99aaaedc6349f545dd934c6315549345dcddb828
Rodgerwilco <crazyonedude@hotmail.com>
2012-05-01 16:26:11 -0700

    #123 - Flax test.

--- GameServer/src/org/moparscape/msc/gs/phandler/PlayerLogin.java
+++ GameServer/src/org/moparscape/msc/gs/phandler/PlayerLogin.java
@@ -237,6 +237,7 @@ public class PlayerLogin implements PacketHandler {
 				player.getInventory().add(new InvItem(189));
 				player.getInventory().add(new InvItem(11, 100));
 				player.getInventory().add(new InvItem(517, 100));
+				player.getInventory().add(new InvItem(675, 1));
 				//end of test items
 				player.getActionSender().sendInventory();
 				player.setChangingAppearance(true);
