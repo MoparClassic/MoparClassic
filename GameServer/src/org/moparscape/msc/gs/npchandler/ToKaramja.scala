1a5e6b76c5f438989b54f4459bd418d99d5f548b
Joe0 <joe.pritzel@gmail.com>
2012-09-04 00:06:29 -0500

    ToKaramja now uses Transact

--- GameServer/src/org/moparscape/msc/gs/npchandler/ToKaramja.scala
+++ GameServer/src/org/moparscape/msc/gs/npchandler/ToKaramja.scala
@@ -3,18 +3,17 @@ import org.moparscape.msc.gs.model.dialog.NpcDialog
 
 class ToKaramja extends NpcDialog {
 	override def init {
-		this + new GenericEnd("Yes please", npc, player) {
-			override def begin {
-				if (player.getInventory.remove(10, 30)) {
-					this >> "You pay 30 gold"; pause(1300)
-					this >> "You board the ship"; pause(1000)
-					player.teleport(324, 713, false); pause(1500)
-					this >> "The ship arrives at Karamja"
-				} else {
-					this < "Oh dear I don't seem to have enough money"
-				}
-				super.begin
+		
+		this + new Transact("Yes please", Array(10 -> 30), Array(), Array("Oh dear I don't seem to have enough money"), npc, player) {
+
+			override def success {
+				this >> "You pay 30 gold"; pause(1300)
+				this >> "You board the ship"; pause(1000)
+				player.teleport(324, 713, false); pause(1500)
+				this >> "The ship arrives at Karamja"
+				super.success
 			}
+			
 		}
 
 		this + new GenericEnd("No thankyou", npc, player)
