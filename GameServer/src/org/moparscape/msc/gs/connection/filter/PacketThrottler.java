509003ca316fbef5499737604bc73d9ed7f7ae74
Rodgerwilco <crazyonedude@hotmail.com>
2013-09-10 13:33:17 -0700

    Readded PacketThrottler for testing.

--- GameServer/src/org/moparscape/msc/gs/connection/filter/PacketThrottler.java
+++ GameServer/src/org/moparscape/msc/gs/connection/filter/PacketThrottler.java
@@ -76,29 +76,29 @@ public class PacketThrottler extends IoFilterAdapter {
 
         // TODO: FIX ME.
 
-//		Player player = (Player) session.getAttachment();
-//		if (session.isClosing() || player.destroyed()) {
-//			return;
-//		}
-//
-//		int count = incrementAndGet(player.getUsernameHash());
-//
-//		if (count > Config.PACKET_PER_SECOND_THRESHOLD) {
-//
-//			if (Config.PACKET_PER_SECOND_ALERT) {
-//				// If the player is initialized, then use the username,
-//				// otherwise use the IP.
-//				String s = (player.isInitialized() ? player.getUsername()
-//						: player.getCurrentIP())
-//						+ " has exceeded the packet per second threshold";
-//				// Sends an alert with a priority of 2.
-//				AlertHandler.sendAlert(s, 2);
-//			}
-//
-//			// Destroys the user and discards the packet.
-//			player.destroy(true);
-//			return;
-//		}
+		Player player = (Player) session.getAttachment();
+		if (session.isClosing() || player.destroyed()) {
+			return;
+		}
+
+		int count = incrementAndGet(player.getUsernameHash());
+		System.out.println(count);
+		if (count > Config.PACKET_PER_SECOND_THRESHOLD) {
+
+			if (Config.PACKET_PER_SECOND_ALERT) {
+				// If the player is initialized, then use the username,
+				// otherwise use the IP.
+				String s = (player.isInitialized() ? player.getUsername()
+						: player.getCurrentIP())
+						+ " has exceeded the packet per second threshold";
+				// Sends an alert with a priority of 2.
+				AlertHandler.sendAlert(s, 2);
+			}
+
+			// Destroys the user and discards the packet.
+			player.destroy(true);
+			return;
+		}
 
 		nextFilter.messageReceived(session, message);
 	}
