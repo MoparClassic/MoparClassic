02f87c42f85e083ca332bec60955f3e268d177f2
Joe0 <joe.pritzel0@gmail.com>
2014-06-18 13:24:27 -0500

    Added useful methods to KillDeathHistory.

--- GameServer/src/org/moparscape/msc/gs/model/player/attribute/KillDeathHistory.scala
+++ GameServer/src/org/moparscape/msc/gs/model/player/attribute/KillDeathHistory.scala
@@ -3,4 +3,11 @@ package org.moparscape.msc.gs.model.player.attribute
 class KillDeathHistory(
 	var npcKills : Long = 0, var npcDeaths : Long = 0,
 	var playerKillsWild : Long = 0, var playerDeathsWild : Long = 0,
-	var playerKillsDuel : Long = 0, var playerDeathsDuel : Long = 0)
+	var playerKillsDuel : Long = 0, var playerDeathsDuel : Long = 0) {
+
+	def getTotalKills = npcKills + getPvPKills
+	def getTotalDeaths = npcDeaths + getPvPDeaths
+
+	def getPvPKills = playerKillsWild + playerKillsDuel
+	def getPvPDeaths = playerDeathsWild + playerDeathsDuel
+}
