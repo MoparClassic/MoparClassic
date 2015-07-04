58c1a808fb3b1b953d4a15d000e5fdabfc33606d
Joe0 <joe.pritzel0@gmail.com>
2015-07-04 16:02:37 -0500

    Started working on #111. Needed to reload shops at runtime, so I added the ::reloadshops command.

--- GameServer/src/org/moparscape/msc/gs/npchandler/ShopKeeper.scala
+++ GameServer/src/org/moparscape/msc/gs/npchandler/ShopKeeper.scala
@@ -1,35 +1,38 @@
 package org.moparscape.msc.gs.npchandler
 import org.moparscape.msc.gs.model.dialog.NpcDialog
 import org.moparscape.msc.gs.Instance
+import org.moparscape.msc.gs.util.Logger
 
 class ShopKeeper extends NpcDialog {
 
-	override def init {
-		val shop = Instance.getWorld.getShop(npc.getLocation)
-		var options = shop.options
+  override def init {
+    val shop = Instance.getWorld.getShop(npc.getLocation)
+    var options = shop.options
+    this + new GenericEnd(options(0).split("\n")(0), npc, player) {
+      override def begin {
+        breath
+        options(0) split "\n" drop 1 foreach { text => this > text; breath }
+        player.setAccessingShop(shop)
+        player.getActionSender.showShop(shop)
+        shop.addPlayer(player)
+        super.begin
+      }
+    }
 
-		this + new GenericEnd(options(0), npc, player) {
-			override def begin {
-				breath
-				player.setAccessingShop(shop)
-				player.getActionSender.showShop(shop)
-				shop.addPlayer(player)
-				super.begin
-			}
-		}
+    options drop 1 foreach (e => this + new GenericEnd(e, npc, player) {
+      override def begin {
+        breath
+        this.optionText split "\n" drop 1 foreach { text => this > text; breath }
+        super.begin
+      }
+    })
+  }
 
-		options = options.drop(1)
+  override def begin {
+    val shop = Instance.getWorld.getShop(npc.getLocation)
 
-		options foreach (e => this + new GenericEnd(e, npc, player))
-	}
+    shop.greeting filterNot ("" ==) split "\n" foreach { text => this > text; breath }
 
-	override def begin {
-		val shop = Instance.getWorld.getShop(npc.getLocation)
-
-		this > shop.greeting
-
-		breath
-
-		end
-	}
+    end
+  }
 }
